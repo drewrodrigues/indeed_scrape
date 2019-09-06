65fb234dbcea0800d36f4542165b279bd1f60d2d
Drew Rodrigues <thesimpledev@gmail.com>
2019-09-05 17:36:15 -0700

    Improve lookup time of Storage

--- lib/storage.rb
+++ lib/storage.rb
@@ -2,38 +2,50 @@ require 'yaml'
 require 'colorize'
 require_relative 'job_posting'
 
+# manage storage of job postings
 class Storage
-  attr_reader :matches
+  attr_reader :matches, :misses
 
   def initialize
     @matches = YAML.load(File.read('./storage/matches.yml'))
     @misses = YAML.load(File.read('./storage/misses.yml'))
-    @matches = [] unless @matches.is_a?(Array)
-    @misses = [] unless @misses.is_a?(Array)
+    @matches = {} unless @matches.is_a?(Hash)
+    @misses = {} unless @misses.is_a?(Hash)
   end
 
-  def save_match(job_posting)
-    @matches << job_posting unless already_saved?(job_posting)
-    File.open('./storage/matches.yml', 'w') { |f| f.write(@matches.to_yaml) }
+  def add(job_postings)
+    job_postings.each do |job|
+      job.passing_score? ? save_match(job) : save_miss(job)
+    end
   end
 
-  def save_miss(job_posting)
-    @misses << job_posting unless already_saved?(job_posting)
-    File.open('./storage/misses.yml', 'w') { |f| f.write(@misses.to_yaml) }
+  def save_match(job)
+    return if already_saved?(job)
+
+    matches[job.id] = job
+    File.open('./storage/matches.yml', 'w') { |f| f.write(matches.to_yaml) }
   end
 
-  def already_saved?(id)
-    @misses.any? { |job| job.id == id } || @matches.any? { |job| job.id == id }
+  def save_miss(job)
+    return if already_saved?(job)
+
+    misses[job.id] = job
+    File.open('./storage/misses.yml', 'w') { |f| f.write(misses.to_yaml) }
+  end
+
+  def already_saved?(job)
+    misses[job.id] || matches[job.id]
   end
 
   def save
-    File.open('./storage/matches.yml', 'w') { |f| f.write(@matches.to_yaml) }
-    File.open('./storage/misses.yml', 'w') { |f| f.write(@misses.to_yaml) }
+    File.open('./storage/matches.yml', 'w') { |f| f.write(matches.to_yaml) }
+    File.open('./storage/misses.yml', 'w') { |f| f.write(misses.to_yaml) }
   end
 
   def move_from_matches_to_misses(job_posting)
-    deleted = @matches.reject! { |posting| posting == job_posting }
+    deleted = matches.reject! { |posting| posting == job_posting }
     raise 'Failed to delete match' unless deleted
-    @misses << job_posting
+
+    misses << job_posting
   end
 end

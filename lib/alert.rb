b9c1a64fb9cbac987d4a8012b758f5bd5ff20a45
Drew <thesimpledev@gmail.com>
2019-09-26 16:54:52 -0700

    Add simple output to alert

--- lib/alert.rb
+++ lib/alert.rb
@@ -1,30 +1,75 @@
 require 'colorize'
+require_relative '../config/settings'
+
+# TODO: use decorator design pattern to do a simple & debugging alerts
 
 # responsible for formatting alerts based upon some conditions
 class Alert
   def self.of_pass_of_fail_for(job)
     if job.passing_score?
-      puts "Passed, '#{job.position}' with points: #{job.points}!"
-        .black
-        .on_green
+      if SETTINGS[:simple_output]
+        print ' √ '.green
+      else
+        puts "Passed, '#{job.position}' with points: #{job.points}!"
+          .black
+          .on_green
+      end
     else
-      puts "Failed, '#{job.position}' with points: #{job.points}.".red
+      if SETTINGS[:simple_output]
+        print ' X '.red
+      else
+        puts "Failed, '#{job.position}' with points: #{job.points}.".red
+      end
     end
   end
 
   def self.already_saved
-    puts 'Already saved!'.light_blue
+    if SETTINGS[:simple_output]
+      print ' → '
+    else
+      puts 'Already saved!'.light_blue
+    end
   end
 
   def self.prime
-    puts 'Is prime, skipping.'.yellow
+    if SETTINGS[:simple_output]
+      print ' → '
+    else
+      puts 'Is prime, skipping.'.yellow
+    end
   end
 
-  def self.next_page(page_number)
-    puts "Going to page: #{page_number}".white.on_magenta
+  def self.on_page(page_number)
+    if SETTINGS[:simple_output]
+      print "\nPage: #{page_number} ".magenta
+    else
+      puts "On page: #{page_number}".white.on_magenta
+    end
   end
 
   def self.timeout(message)
-    puts message.red.on_white
+    if SETTINGS[:simple_output]
+      print ' X '.red.on_white
+    else
+      puts message.red.on_white
+    end
+  end
+
+  def self.bad_position(position)
+    if SETTINGS[:simple_output]
+      print ' → '
+    else
+      puts "Bad position (#{position}), skipping".light_blue
+    end
+  end
+
+  def self.start_search(position, location)
+    if SETTINGS[:simple_output]
+      puts "Searching #{position} @ #{location} ".magenta
+    else
+      puts '-' * 20
+      puts "Searching: #{position} @ #{location}"
+      puts '-' * 20
+    end
   end
 end

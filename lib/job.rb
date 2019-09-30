d5ceda74147d85cbe12983879345f628001d4908
Drew <thesimpledev@gmail.com>
2019-09-30 08:06:38 -0700

    Add review status validation

--- lib/job.rb
+++ lib/job.rb
@@ -1,6 +1,9 @@
 require 'active_record'
 
 class Job < ActiveRecord::Base
+  REVIEW_STATUSES = %w(needs_review applied bad_match dont_want interested response)
+
   validates :company, :description, :job_id, :location, :position,
             :review_status, :url, presence: true
+  validates :review_status, inclusion: { in: REVIEW_STATUSES }
 end

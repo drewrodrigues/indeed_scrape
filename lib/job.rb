fe9596a262fd94bd920a1bb5b41170a85de67d5f
Drew <thesimpledev@gmail.com>
2019-10-03 16:32:36 -0700

    Remove job_posting and migrate methods to Job

--- lib/job.rb
+++ lib/job.rb
@@ -1,10 +1,53 @@
 require 'active_record'
+require 'colorize'
+require_relative 'point_allocation'
+
+ActiveRecord::Base.establish_connection(
+    adapter: 'sqlite3',
+    database: './job_search.db'
+)
 
 class Job < ActiveRecord::Base
+  after_initialize :set_point_allocation
+  attr_reader :point_allocation
+
+  delegate :points, :passing_score?, :good_matches, to: :point_allocation
+
   VALID_STATUSES = %w(needs_review applied bad_match dont_want interested response)
   enum status: VALID_STATUSES
 
   validates :company, :description, :job_id, :location, :position,
-            :review_status, :url, presence: true
+            :status, :url, presence: true
   validates :status, inclusion: { in: VALID_STATUSES }
+
+  def review_status_pretty
+    color = if bad_match?
+              :red
+            elsif applied?
+              :green
+            elsif needs_review?
+              :yellow
+            elsif interested?
+              :light_blue
+            elsif dont_want?
+              :red
+            elsif response?
+              :green
+            else
+              ''
+            end
+
+    status.to_s.send(color)
+  end
+
+  def to_s
+    puts "#{position} @ #{company}".colorize(background: :green, color: :black)
+
+    puts '> Description'.colorize(background: :white, color: :black)
+    puts description
+  end
+
+  def set_point_allocation
+    @point_allocation = PointAllocation.new(description)
+  end
 end

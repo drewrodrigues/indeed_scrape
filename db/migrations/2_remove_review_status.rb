25a357a0bf991aef189e6ef96f9f120d728de9ec
Drew <thesimpledev@gmail.com>
2019-10-24 16:15:38 -0700

    Remove extra migrations

--- db/migrations/2_remove_review_status.rb
+++
@@ -1,8 +0,0 @@
-require 'active_record'
-
-require_relative '../connection'
-require_relative '../../lib/indeed_scrape/job'
-
-ActiveRecord::Schema.define do
-  remove_column :jobs, :review_status, :string, null: false
-end
